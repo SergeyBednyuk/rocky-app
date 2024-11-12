ec6805c49e76f844a2a072b78bb83bb6a3e37ab2
Siarhei Biadniuk <sergeybednyuk@gmail.com>
2024-11-12 16:09:34 +0100

    Add session to project. Create extension for get/set session.

--- Rocky-app/Program.cs
+++ Rocky-app/Program.cs
@@ -14,6 +14,13 @@ public class Program
         builder.Services.AddDbContext<AppDbContext>(options => 
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
             );
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(10);
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+        });
         
         var app = builder.Build();
 
@@ -31,7 +38,8 @@ public class Program
         app.UseRouting();
 
         app.UseAuthorization();
-
+        
+        app.UseSession();
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
